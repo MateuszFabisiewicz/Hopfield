54c035c35bb8b6f74f05feae42b9a057a793c1f1
agoworek <amadeo.go@vp.pl>
2023-11-20 22:57:53 +0100

    Recall visualization

--- hopfield.py
+++ hopfield.py
@@ -31,15 +31,20 @@ class HopfieldNetwork:
                             self.weights[i, j] += pattern[j] * (pattern[i] - weights[i, j] * pattern[j])
             self.weights /= len(patterns)
 
-    def recall(self, pattern, max_iters=100, show_details = False):
+    def recall(self, pattern, max_iters=100, show_details = False, plotShape = None):
         if pattern.shape != (self.num_neurons,):
             raise ValueError("Pattern shape does not match network size.")
         
         for _ in range(max_iters):
             weighted_sum = np.dot(self.weights, pattern)
             new_pattern = np.sign(weighted_sum) + (weighted_sum==0) 
+
+            if(plotShape):
+                self.plot_patterns_as_bitmap(new_pattern, plotShape)
+
             if(show_details):
                 print(new_pattern)
+
             if np.array_equal(new_pattern, pattern):
                 return new_pattern
             pattern = new_pattern
@@ -90,22 +95,11 @@ class HopfieldNetwork:
         return False
 
     
-    def plot_patterns_as_bitmap(self, patterns, image_size=(32, 32), save_path='patterns.bmp'):
-        num_patterns = len(patterns)
-        pattern_size = int(np.sqrt(self.num_neurons))
-        cols = num_patterns
-        rows = 1
-        bitmap = np.ones((rows * pattern_size, cols * pattern_size), dtype=np.uint8) * 255
-        
-        for i in range(num_patterns):
-            pattern = (patterns[i].reshape(pattern_size, pattern_size) + 1) * 127
-            row = i // cols
-            col = i % cols
-            bitmap[row * pattern_size:(row + 1) * pattern_size, col * pattern_size:(col + 1) * pattern_size] = pattern
-        
-        plt.imsave(save_path, bitmap, cmap='gray')
-        plt.show()
-        
-        plt.imsave(save_path, bitmap, cmap='gray')
-        plt.show()
+    def plot_patterns_as_bitmap(self, pattern, shape):
+        fig = plt.figure(figsize=(shape[0]/5,shape[1]/5))
+        pattern_array = np.array(pattern)
+        pattern_array = pattern_array.reshape(shape)
+        bitmap = np.where(pattern_array == 1, 255, 0)
+        plt.axis("off")
+        plt.imshow(bitmap, cmap='gray', interpolation='nearest', aspect="auto")
 
